1f9b8874d22564d24fa5e5a09d895d5299bd7816
Fatin Mojumder <fatinm1@umbc.edu>
2024-01-01 00:00:00 -0500

    Contribution on 2024-01-01

--- populate.py
+++ populate.py
@@ -6,8 +6,8 @@ from datetime import datetime, timedelta
 repo_path = r"C:\Users\GAMER\Nearest-Neighbor-Finder"  # Use raw string to fix backslash issue
 file_name = "contribution_file.txt"  # A file to modify in the repo
 start_date = "2024-01-01"  # Start date for contributions (last three months)
-end_date = "2024-01-31"  # End date for contributions
-commits_per_day = 43  # Number of commits to make per day
+end_date = "2024-01-10"  # End date for contributions
+commits_per_day = 21  # Number of commits to make per day
 
 def run_git_command(command, cwd):
     """Run a git command in the specified directory."""
